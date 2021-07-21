56c1db2983930012679babc7a00b824222c628a8
scriptSQD <scriipt.fun@gmail.com>
2021-07-21 22:08:17 +0300

    fix regex undefined behavior, fully working script

--- source.cpp
+++ source.cpp
@@ -7,19 +7,38 @@
 // external
 #include "argh.h"
 
-void replaceValue(std::string replaceIn, std::string varName, std::string replaceTo) {
-    replaceIn.replace(replaceIn.find(varName) + varName.size() + 1, 1, replaceTo);
+std::string tmp = "";
+
+std::string replaceValue(std::string replaceIn, std::string varName, std::string replaceTo) {
+    if (varName != "CONFIG_UEVENT_HELPER_PATH") {
+        replaceIn.replace(replaceIn.find(varName) + varName.size() + 1, 1, replaceTo);
+        return replaceIn;
+    }
+    else {
+        replaceIn.replace(replaceIn.find(varName) + varName.size() + 1, 1, "\"\"");
+        return replaceIn;
+    }
 }
 
-void setValue(std::string setIn, std::string varName, std::string setTo) {
-    setIn.replace(setIn.find(varName) + varName.size(), 11, "="+setTo);
-    setIn.replace(setIn.find(varName) - 2, 2, "");
+std::string setValue(std::string setIn, std::string varName, std::string setTo) {
+    if (setIn.find(varName + " is not set") != std::string::npos) {
+        setIn.replace( (setIn.find(varName) + varName.length() ), 11, "="+setTo);
+        setIn.replace( (setIn.find(varName) - 2), 2, "");
+        return setIn;
+    }
+    else {
+        tmp = varName + "=" + setTo + "\n";
+        setIn.append(tmp);
+        return setIn;
+    }
 }
 
 int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
     std::string targetArg;
     std::filesystem::path targetConfig;
-    std::string execPath(std::filesystem::path(argv[0]).parent_path().string());
+    std::filesystem::path execPath(std::filesystem::path(argv[0]).parent_path());
+    system( ( "cd " + execPath.string() ).c_str() );
     bool verbose = false;
     bool erronly = false;
     {   // starter argument check 'n' parse
@@ -33,55 +52,62 @@ int main(int argc, char** argv) {
         else {
             targetConfig = targetArg;
             if (std::filesystem::exists(targetConfig)) {
-                std::cout << targetConfig.string() << std::endl;        // debug line
+                // std::cout << targetConfig.string() << std::endl;        // debug line
             }
             else {
                 fprintf(stderr, "Target does not exist!\n");
             }
         }
         if (arg[{"-v", "--verbose"}]) {
-            std::cout << "Verbose mode enabled\n";        // debug line
-            //somehow define that everything outputs to user
+            // std::cout << "Verbose mode enabled\n";        // debug line
             verbose = true;
         }
         if (arg[{"-e", "--errors-only"}]) {
-            std::cout << "Fixing only \"ERROR\" entries...\n";        // debug line
+            // std::cout << "Fixing only \"ERROR\" entries...\n";        // debug line
             erronly = true;
         }
     }
-    // targetConfig = "/home/sqd/Desktop/halium-repo/kernel/samsung/universal7580/arch/arm64/configs/lineageos_a3xelte_defconfig"; // debug line
 
-    std::string mkvc = (execPath + "mer_kcc/mer_verify_kernel_config " + targetConfig.string() + " > " + execPath + "mkvc_res.txt");
+    std::string mkvc = ("mer_kcc/mer_verify_kernel_config " + targetConfig.string() + " > " + "mkvc_res.txt");
     system(mkvc.c_str());
 
-    std::ifstream res(execPath + "mkvc_res.txt", std::ios::in | std::ios::binary);
+    std::ifstream res("mkvc_res.txt", std::ios::in | std::ios::binary);
     if (!res.is_open()) {
-        fprintf(stderr, "Failed to open \"mer-kernel-verify-config\" output");
+        fprintf(stderr, "Failed to open \"mer-kernel-verify-config\" output (mkvc_res.txt)");
         return -1;
     }
     std::string mkvcOut(std::istreambuf_iterator<char>(res), std::istreambuf_iterator<char>{});
     res.close();
 
-    std::regex regex(erronly ? R"(ERROR:\s(.*) is invalid\n(.*)\nAllowed values\s*:\s*(.).*\n.*)" : R"(:\s(.*) is invalid\n(.*)\nAllowed values\s*:\s*(.).*\n.*)");
+    std::regex regex(erronly ? "ERROR:\\s(.*) is invalid\\n(.*)\\nAllowed values\\s*:\\s*(.).*\\n.*" : ":\\s(.*) is invalid\\n(.*)\\nAllowed values\\s*:\\s*(.).*\\n.*");
     std::smatch match;
 
     std::ifstream kernelCfg(targetConfig.string(), std::ios::in | std::ios::binary);
     std::string cfg (std::istreambuf_iterator<char>(kernelCfg), std::istreambuf_iterator<char>{});
+    cfg.append("\n");
     kernelCfg.close();
 
     while (std::regex_search(mkvcOut, match, regex)) {
         // match[1-3] contain: 1: kernel config var name, 2: cause of error/warning, 3:allowed value nm.1
-        mkvcOut = match.suffix().str();
-        match[2].str().find("unset") ? setValue(cfg, match[1], match[3]) : replaceValue(cfg, match[1], match[3]);
+        if (match[2].str().find("unset") != std::string::npos) {
+            cfg = setValue(cfg, match[1].str(), match[3].str());
+        }
+        else {
+            cfg = replaceValue(cfg, match[1].str(), match[3].str());
+        }
         if (verbose) {
-            std::cout << "Either setting or replacing " + match[1].str() + " value to " + match[3].str() + ".\n";
+            std::cout << "Either setting or replacing " << match[1].str() << " value to " << match[3].str() << ".\n";
+        }
+        mkvcOut = match.suffix().str();
+    }
+    std::cout << cfg << std::endl;
+    if (!std::filesystem::exists("output")) {
+        if (!std::filesystem::create_directory(std::filesystem::path("output"))) {
+            fprintf(stderr, "Failed to create \"output\" directory...\n");
+            return -1;
         }
     }
-
-    if (!std::filesystem::create_directory(std::filesystem::path(argv[0]).parent_path() / "output")) {
-        fprintf(stderr, "Failed to create \"output\" directory...\n");
-    }
-    std::ofstream cfgOut(execPath + "output/" + "defconfig", std::ios::out | std::ios::binary);
+    std::ofstream cfgOut((execPath / "output" / "defconfig").string(), std::ios::out | std::ios::binary);
     cfgOut << cfg;
     cfgOut.close();
 
